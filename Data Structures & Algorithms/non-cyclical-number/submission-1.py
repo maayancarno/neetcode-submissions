21c9032dbf943afc6db72cd9fefc55061b092e58
maayancarno <maayan.carno@outlook.com>
2026-06-13 12:17:36 +0100

    Add: non-cyclical-number - submission-1

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-1.py
@@ -0,0 +1,42 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+
+        def getSumOfSquares(num:int) -> int:
+            sum_squares = 0
+
+            while num > 0:
+                d = num % 10
+                d_squared = d * d
+                sum_squares += d_squared
+                num = num//10 
+            
+            return sum_squares
+        
+        slow = n
+        fast = n
+
+        while True:
+            slow = getSumOfSquares(slow)
+            fast = getSumOfSquares(getSumOfSquares(fast))  
+
+            if fast == 1:
+                return True
+
+            if slow == fast:
+                return False
+
+
+
+"""
+Time complexity: 
+
+O(logn)
+Space complexity: 
+O
+(
+1
+)
+O(1)
+
+n is the value of the input integer.
+"""
